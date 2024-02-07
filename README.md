90601fe825465c0a7189ffb2e7e11d66c5fd77dc
Rob Ireton <rob@ireton.org>
2024-02-07 16:07:20 -0500

    speculative configuration

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # vuur
 
 a forward-looking Node.js FHIR client module
+
+## Resources
+* [HL7 FHIR Release 4](http://hl7.org/fhir/R4/)
+* [Epic on FHIR](https://fhir.epic.com)
--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 export default class FHIR {
   #config
+  #baseURL
+  #clientID
+  #publicKey
 
   constructor (config) {
     this.#config = config
+    if ('baseURL' in config) this.#baseURL = new URL(config.baseURL)
+    if ('clientID' in config) this.#clientID = config.clientID
+    if ('publicKey' in config) this.#publicKey = config.publicKey
   }
 }
